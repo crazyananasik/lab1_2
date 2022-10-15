77e850b82ba35bbada061d39365f2d91aacdef46
pibo0121034 <pibo0121034@WIN-JUB8CCCU1GA>
2022-10-15 05:35:49 +0300

    Add project files.

---
+++ lab1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int year;
+            string tmp;
+            string pause;
+            Console.WriteLine("Введите число:");
+            tmp = Console.ReadLine();
+            year = Convert.ToInt32(tmp);
+            Console.WriteLine($"Введеный год: {year}");
+
+            if (year % 4 == 0)
+            {
+                Console.WriteLine("Год високосный");
+            }
+            else
+            {
+                Console.WriteLine("Год не високосный");
+            }
+            pause = Console.ReadLine();
+        }
+    }
+}
